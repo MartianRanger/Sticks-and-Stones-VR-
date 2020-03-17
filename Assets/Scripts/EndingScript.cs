f94be269c898159d9bf406e3bc025739f4b427d3
MartianRanger <35358754+MartianRanger@users.noreply.github.com>
2020-03-16 21:00:16 -0600

    Done with You Win Scene

---
+++ Assets/Scripts/EndingScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScript : MonoBehaviour
+{
+    public void DestroyGameObject(GameObject enemy)
+    {
+        Destroy(enemy);
+    }
+
+}
